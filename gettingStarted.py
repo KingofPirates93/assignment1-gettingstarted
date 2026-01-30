f62b9420d9aaaca602f58ef6172ad50b43ce7dc0
Swapnil Patel <swapnilpatel@Swapnils-MBP.lan>
2026-01-30 17:32:14 -0500

    finished getting started...

--- gettingStarted.py
+++ gettingStarted.py
@@ -3,17 +3,26 @@
 ### Output - The right answer for the specific question.
 
 def welcome_assignment_answers(question):
-    #Students do not have to follow the skeleton for this assignment.
-    #Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap"
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
-    else: 
-        ### you should understand why this else case should be included
-        ### what happens if there is a typo in one of the questions?
-        ### maybe put something here to flag an issue and catch errors
-        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        answer = "25eecbc4af61a8375d198babd9b86fba1475135f1c36d41efd5937f40345c10e"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = 4
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = 2
+    else:
+        answer = "Question not recognized"
     return(answer)
 # Complete all the questions.
 
